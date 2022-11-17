70e4a7872fc7308a6520952afa72c8650debaf85
yaerhee <yaerhee@naver.com>
2022-11-18 00:51:58 +0900

    CSS module import & backtick className(s) - not recommended

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,20 @@
 import Link from 'next/link'
 import {useRouter} from "next/router";
+import styles from './navbar.module.css' // style module import -> usage as Object.property
 
 export default function Navbar() {
     const router = useRouter()
 
     return (
         <nav>
-            <Link href="/" style={{ color: router.pathname == "/" ? 'red' : 'blue' }}>
+            {/* className 에 random text 를 추가해서 HTML return -> No Duplicates!! */}
+            <Link href="/" className={`${styles.link} 
+                                ${router.pathname == "/" ? styles.active : ''}`}>
                 Home
             {/* don't use <a> tag from 13 version! */}
             </Link>
-            <Link href="/about" style={{ color: router.pathname == "/about" ? 'red' : 'blue' }}>
+            <Link href="/about" className={`${styles.link} 
+                                ${router.pathname == "/about" ? styles.active : ''}`}>
                 About
             </Link>
         </nav>
