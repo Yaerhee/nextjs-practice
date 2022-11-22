d6c21a8877d8cb834dd71836310d3049fde57111
yaerhee <yaerhee@naver.com>
2022-11-23 00:57:27 +0900

    SSR(getServerSideProps), type 정의 및 리팩토링

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Title from "../components/Title";
-import {useEffect, useState} from "react";
+import {GetServerSideProps, InferGetServerSidePropsType} from "next";
 
 interface IMovieProps {
     id: number;
@@ -7,19 +7,11 @@ interface IMovieProps {
     original_title: string;
 }
 
-export default function Index() {
-    const [movies, setMovies] = useState([])
-    useEffect(() => {
-        (async () => {
-            const {results} = await (await fetch(`/api/movies`)).json()
-            setMovies(results)
-        })()
-    }, [])
+export default function Index({ results }: InferGetServerSidePropsType<GetServerSideProps>) {
     return (
         <div className="container">
             <Title title="Home"/>
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie: IMovieProps) => (
+            {results?.map((movie: IMovieProps) => (
                 <div className="movie" key={movie.id}>
                     <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
                     <h4>{movie.original_title}</h4>
@@ -56,4 +48,18 @@ export default function Index() {
             </style>
         </div>
     )
+}
+
+// get Server Site Props
+export async function getServerSideProps() {
+    // 이름 변경 불가! 서버에서 돌아가게 하는 코드
+    // SSR 을 요청하기 때문에, 백엔드에서 실행되므로 이쪽에서 API_KEY 를 숨기는 방법도 있음
+    const { results } = await (
+        await fetch(`http://localhost:3000/api/movies`)).json()
+    return {
+        props: {
+            results, // 서버에서 받아들인 결과물을 그대로 통째로 출력해줌
+            // 데이터가 유효해야 화면이 출력 (Loading 이 있을 필요는 없지만, 로딩이 길 경우 문제)
+        }
+    }
 }
